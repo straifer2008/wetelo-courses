b28ae69943afd609fcc923807d1abecab6a4badf
artem@wetelo.com <mamamia1991>
2019-02-25 09:25:44 +0200

    First commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={{
+          flex: 1,
+          backgroundColor: '#fff',
+          alignItems: 'center',
+          justifyContent: 'center',
+      }}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
+}
